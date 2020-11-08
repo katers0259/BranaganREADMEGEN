476f9bac07b98522b322ca5b6270709d7d6067a6
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2020-11-08 13:59:19 -0700

    finished questions

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = requir('fs');
+const util = require('uti');
 
+const markdown = require('./utils/generateMarkdown');
+
+
+// array of questions for user
+const questions = [{
+    type: "input",
+    message: "What is your projects title?",
+    name: "Title",
+},
+{
+    type:"input",
+    message: "Give a description of what the project is about",
+    name: "Description",
+},
+{
+    type:"checkbox",
+    message: "Choose your table of contents items",
+    name: "stack",
+    choices: ['Title','Description','Usage','License','Contributors'],
+},
+{
+    type:"input",
+    message:"How do you use the app, and what do you need to install?",
+    name: "Usage",
+},
+{
+    type: "input",
+    message: "Who worked on this?",
+    name: "Contributors",
+},
+{
+    type:"input",
+    message:"What license are you using?",
+    name: "License",
+},
+{
+    type:"input",
+    message:"What is your Github username?",
+    name:"Username",
+},
+{
+    type:"input",
+    message:"What is your email address?",
+    name:"Email",
+},
 ];
 
 // function to write README file
